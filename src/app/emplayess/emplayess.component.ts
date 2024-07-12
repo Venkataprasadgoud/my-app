974930656a35a97070ceb5bd91d1c18bb76ddcff
Person 1 <venkataprasadgoud7079@gmail.com>
2024-07-12 10:54:28 +0530

    name add emplyess

--- src/app/emplayess/emplayess.component.ts
+++ src/app/emplayess/emplayess.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmplayessComponent implements OnInit {
   public employees:any = [
-    {name:'Durgesh',age:25,dob:new Date('12/30/1998'),experience:2,package:6},
+    {name:'raju',age:25,dob:new Date('12/30/1998'),experience:2,package:6},
     {name:'Venkat',age:35,dob:new Date('10/22/1988'),experience:8,package:18},
-    {name:'Arif',age:23,dob:new Date('1/23/1999'),experience:3,package:9},
-    {name:'Kranthi',age:30,dob:new Date('6/17/1992'),experience:4.5,package:12},
+    {name:'ravi',age:23,dob:new Date('1/23/1999'),experience:3,package:9},
+    {name:'banu',age:30,dob:new Date('6/17/1992'),experience:4.5,package:12},
   ]
 
        
